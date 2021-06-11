45c3c237375e6d4a9dcbb0eff3a9c4f47bd9800f
lionarmy <43376882+lionarmy@users.noreply.github.com>
2021-06-11 17:35:42 +0530

    Delete Day-3 directory

--- Day-3/square.py
+++
@@ -1,7 +0,0 @@
-digit = int(input("Enter an integer number: "))
-
-if digit < 10:
-    square = digit*digit
-    print(f"Square of {digit} is {square}")
-else :
-    print ("number is not less than 10")
--- Day-3/swap.py
+++
@@ -1,9 +0,0 @@
- x = input('Enter value of x: ')  
-y = input('Enter value of y: ')  
-   
-temp = x  
-x = y  
-y = temp  
-  
-print('The value of x after swapping: {}'.format(x))  
-print('The value of y after swapping: {}'.format(y))
